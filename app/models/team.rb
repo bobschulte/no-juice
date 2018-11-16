86456da09daee4d235715bac2eac4f8bf3f3a89f
Robert Schulte <schulte.robertw@outlook.com>
2018-11-16 14:50:53 -0600

    added comments to Team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
-  has_many :games, foreign_key: 'home_team_id', class_name: 'Game'
-  has_many :games, foreign_key: 'away_team_id', class_name: 'Game'
+  has_many :games, foreign_key: 'home_team_id', class_name: 'Game' # MAY NOT NEED THIS LINE AT ALL PER BARBARA'S PROJECT
+  has_many :games, foreign_key: 'away_team_id', class_name: 'Game' # MAY NOT NEED THIS LINE AT ALL PER BARBARA'S PROJECT
   validates :location_name, presence: true
   validates :mascot_name, presence: true, uniqueness: true
   validates :abbrev_name, presence: true, uniqueness: true
